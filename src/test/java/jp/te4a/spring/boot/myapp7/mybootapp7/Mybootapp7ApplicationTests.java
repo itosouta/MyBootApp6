53c8e17b4c3724b6995edb241510a4bd4606c560
itosouta <te210832@gmail.com>
2024-07-16 15:45:18 +0900

    課題7

--- src/test/java/jp/te4a/spring/boot/myapp7/mybootapp7/Mybootapp7ApplicationTests.java
+++ src/test/java/jp/te4a/spring/boot/myapp7/mybootapp7/Mybootapp7ApplicationTests.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package jp.te4a.spring.boot.myapp7.mybootapp7;
 
 import org.junit.jupiter.api.Test;
@@ -12,18 +11,3 @@ class Mybootapp7ApplicationTests {
 	}
 
 }
-=======
-package jp.te4a.spring.boot.myapp7.mybootapp7;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Mybootapp7ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
->>>>>>> 1ccd22545fc6801f99d0bee9e15aecad7c365a17
