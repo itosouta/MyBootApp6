ba09a3bb029d7567d0ba39fedc328db4578035fe
itosouta <te210832@gmail.com>
2024-07-23 16:51:38 +0900

    課題11

--- src/main/java/jp/te4a/spring/boot/myapp11/mybootapp11/BookForm.java
+++ src/main/java/jp/te4a/spring/boot/myapp11/mybootapp11/BookForm.java
@@ -8,13 +8,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BookForm {
 private Integer id ;
+
 @NotNull
 @Size(min = 3)
 @TestValid(param = "abc")
 private String title;
+
+
 @Size(min = 3, max = 20)
 private String writter;
+
 private String publisher;
+
 @Min(0)
 private Integer price;
 }
--- src/main/java/jp/te4a/spring/boot/myapp11/mybootapp11/TestValid.java
+++ src/main/java/jp/te4a/spring/boot/myapp11/mybootapp11/TestValid.java
@@ -3,6 +3,8 @@ package jp.te4a.spring.boot.myapp11.mybootapp11;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
